59006669064dc18d168360e619519b914dbeb2b9
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-31 11:08:21 +0600

    token-commit

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.35x3s48.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.35x3s48.mongodb.net`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// Rest of the code...
 
-// Connect to MongoDB on server startup
-async function connectToMongo() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('cerDoctor');
+        const servicesCollection = database.collection('services');
+
+       
+        // auth related api
+        app.post('jwt', async(req, res)=>{
+            const user = req.body;
+            console.log(user)
+            const token  = jwt.sign(user, process.env.ACESS_TOKEN, {expiresIn: '1h'} )
+            res.send(token);
+        })
+
+
+
+        console.log("Connected to MongoDB!");
+    } finally {
+        // The client will close when you finish/error
+    }
 }
 
-// Call the connectToMongo function to establish the MongoDB connection
-connectToMongo();
-
-
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Car Doctor server is running');
+    res.send('Car Doctor server is running');
 });
 
 app.listen(port, () => {
-  console.log(`Car Doctor Server is running on Port: ${port}`);
+    console.log(`Car Doctor Server is running on Port: ${port}`);
 });
+
+
