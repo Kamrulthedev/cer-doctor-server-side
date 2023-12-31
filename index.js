cf0e3051df84332670e3204e24a6ddda8586ac85
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-31 21:02:02 +0600

    commit-commit

--- index.js
+++ index.js
@@ -6,12 +6,39 @@ var jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// use middlerwer
 app.use(cors({
     origin:['http://localhost:5173/'],
     credentials:true
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+
+
+// create middleWares
+const logger = async(req, res, next) =>{
+    console.log('called', req.host, req.originalUrl)
+    next()
+}
+
+// varifyToken 
+const verifyToken = async(req, res, next) =>{
+       const token = req.cookie?.token;
+       if(!token){
+        return res.this.state(401).send({messages: 'not authorized'})
+       }
+       jwt.verify(token, process.env.ACESS_TOKEN, (err, decode)=>{
+           if(err){
+            console.log(err)
+            return res.status(401).send({messages: 'nuauthorizied'})
+           }
+        //if token is valid it would be decded
+           console.log('value in the token', decode)
+           next()
+       })
+}
 
 const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.35x3s48.mongodb.net`;
 
@@ -28,7 +55,7 @@ async function run() {
 
        
         // auth related api
-        app.post('jwt', async(req, res)=>{
+        app.post('jwt', logger, verifyToken, async(req, res)=>{
             const user = req.body;
             console.log(user)
             const token  = jwt.sign(user, process.env.ACESS_TOKEN, {expiresIn: '1h'} )
@@ -55,6 +82,8 @@ async function run() {
         // The client will close when you finish/error
     }
 }
+
+
 
 run().catch(console.dir);
 
